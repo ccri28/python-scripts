416f90c6ca8dcea8290978e7f439d110c76070d4
Cristian Faciano <cristian.faciano28@gmail.com>
2024-05-23 17:54:08 +0200

    Ho sistemato un po' il codice aggiungendo spazi e commenti

--- sconto.py
+++ sconto.py
@@ -1,10 +1,15 @@
 prezzo = input('Inserire prezzo: ')
 sconto = input('Inserire sconto: ')
+
 #faccio il casting in float delle due variabili
 prezzo = float(prezzo)
 sconto = float(sconto)
+
+#faccio il calcolo del prezzo scontato
 scala = prezzo * sconto / 100
 prezzo_scontato = prezzo - scala
+
 #arrotonda il numero a due decimali
+#mostro prezzo scontato
 prezzo_scontato = round(prezzo_scontato,2)
 print(f"Il prezzo scontato è: {prezzo_scontato}")
